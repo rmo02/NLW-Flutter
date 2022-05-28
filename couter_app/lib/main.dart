5135c89d3dd9f080fe4adaf4d6e3e52a88d374ff
rmo02 <ramoon.maia23@gmmail.com>
2022-05-28 14:21:48 -0300

    contador simples em flutter

--- couter_app/lib/main.dart
+++ couter_app/lib/main.dart
@@ -2,10 +2,40 @@ import 'package:flutter/material.dart';
 
 void main () {
   runApp(MaterialApp(
-    home: Container(
-      color: Colors.purple,
-      child: Text(
-        'teste'
-      ), 
-    )));
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {
+      
+    });;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Meu Primeiro App"),
+      ),
+      // ignore: prefer_const_constructors
+      body: Center(child: Text("Contador\n$count", 
+      textAlign: TextAlign.center)),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },),
+    );
+  }
 }
